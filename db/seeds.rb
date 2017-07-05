22100e575d97c416a070065aed8d06c6bf51bc07
davidtom <davidjtomczyk@gmail.com>
2017-07-05 19:10:30 -0400

    complete lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code:"vne452", store:"Chipotle")
+Coupon.create(coupon_code:"mny679", store:"McDonalds")
+Coupon.create(coupon_code:"acx845", store:"Sharper Image")
+Coupon.create(coupon_code:"qpv410", store:"Dick's")
